da12726639946249cd0f26063620fabcb90b6c85
Linus <linus.degen@gibmit.ch>
2018-12-04 11:12:21 +0100

    initialized docker environment

---
+++ php/index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Author: Noah Grun
+ * Date: 04.12.2018
+ */
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name="author" content="Noah Grun, Linus Degen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Title Tag -->
+    <title>Sharebucket</title>
+
+    <!-- Icon -->
+    <link rel="icon" href="img/sb_icon.gif" type="image/gif" sizes="32x32">
+
+    <!-- Bootstrap import -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+
+    <!-- CSS import -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+<body>
+
+    <!-- Navbar with Login -->
+    <nav class="navbar navbar-light bg-dark">
+        <a class="navbar-brand" href="#">
+         <img src="img/sb_logo.png" width="32" height="32" alt="">
+         <span>Sharebucket</span>
+        </a>
+        <div align="right">
+        <span class="right">Einloggen</span>
+        <img src="img/sb_user.png" align="right" sizes="32x32"></img>
+        </div>
+    </nav>
+
+<!-- Title, subtitle -->
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Sharebucket</h1>
+        <p class="lead">Teile deine Projektideen mit uns!</p>
+    </div>
+</div>
+
+
+
+</body>
+</html>
+
